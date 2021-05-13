0c7a5511a7d70dd4f70e9f747d370a9b071aae42
sidmohanty11 <sidmohanty11@gmail.com>
2021-05-13 19:43:25 +0530

    fixed callback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -28,7 +28,7 @@ export async function getStaticPaths() {
 
     (await client).close();
     return {
-        fallback: false,
+        fallback: 'blocking',
         paths: meetups.map(meetup => ({ params: { meetupId: meetup._id.toString() } }))
     }
 }
